71e2de3b17789d81d7c06a9c943202a1528372ec
YllkaGojani <yllka.gojani@gmail.com>
2016-09-07 22:05:34 +0200

    Added the file for the ScoreAndGrade Python assignment.

---
+++ scoreAndGrade.py
@@ -0,0 +1,22 @@
+def scoreAndGrade():
+	for i in range (0, 10):
+		score = input('Your score:')	
+		if(score >= 60) and (score <= 69):
+			grade= 'D'
+			print 'Score:',score,';Your grade is:',grade
+		elif(score >69) and (score <= 79):
+			grade = 'C'
+			print 'Score:',score,';Your grade is:',grade
+		elif(score >79) and (score <= 89):
+			grade = 'B'
+			print 'Score:',score,';Your grade is:',grade
+		elif(score >89) and (score <= 100):
+			grade = 'A'
+			print 'Score:',score,';Your grade is:',grade
+		else:
+			grade = 'undefined'
+			print 'Score:',score,';Your grade is:',grade
+	print 'End of the program. Bye!'		
+
+
+scoreAndGrade()
